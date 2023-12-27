3a539ada81975fe04f550e60172294f708b69ab2
VijayVadaboina <vijayvvadaboina@gmail.com>
2023-12-27 20:40:45 +0530

    added page navigation through event delegation

--- script.js
+++ script.js
@@ -32,6 +32,47 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+/*****************************************************************************************/
+//implementing smooth scrolling to section1
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", function (e) {
+  // implementing old school way
+  // const s1Coord = section1.getBoundingClientRect();
+  // window.scrollTo({
+  //   left: s1Coord.left + window.scrollX,
+  //   top: s1Coord.top + window.scrollY,
+  //   behaviour: "smooth",
+  // });
+
+  // implementing in modern view
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+//Page Navigation
+//1 --> through iterating elements
+//can be implented like but have cons - readme file - navigation problems
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const element_id = this.getAttribute("href");
+//     console.log(element_id);
+//     document.querySelector(element_id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+//2 --> event delegation property
+// 1) add event listener to the common parent
+// 2)determine what element originated the event
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  //Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const element_id = e.target.getAttribute("href");
+    console.log(element_id);
+    document.querySelector(element_id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+/*****************************************************************************************/
 
 //creating DOM element -- creating a cookie type messsage
 const header = document.querySelector("header");
@@ -63,20 +104,3 @@ message.style.justifyContent = "space-between";
 message.style.alignItems = "centre";
 message.style.paddingInlineStart = "100px";
 message.style.paddingTop = "20px";
-
-//implementing smooth scrolling to section1
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
-
-btnScrollTo.addEventListener("click", function (e) {
-  // implementing old school way
-  // const s1Coord = section1.getBoundingClientRect();
-  // window.scrollTo({
-  //   left: s1Coord.left + window.scrollX,
-  //   top: s1Coord.top + window.scrollY,
-  //   behaviour: "smooth",
-  // });
-
-  // implementing in modern view
-  section1.scrollIntoView({ behavior: "smooth" });
-});
